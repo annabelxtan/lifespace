6c54159194bbc51877b0f3abaca9910a0a332715
EstebanRamosFractal <46715376+EstebanRamosFractal@users.noreply.github.com>
2021-11-26 17:00:00 -0500

    Add Examples with mapbox

--- version2/CardinalKit-Example/CardinalKit/Components/Mapbox/ShowMap.swift
+++ version2/CardinalKit-Example/CardinalKit/Components/Mapbox/ShowMap.swift
@@ -23,19 +23,160 @@ struct MapViewWrapper : UIViewControllerRepresentable {
     }
 }
 
-class ShowMap: UIViewController {
+class ShowMap: UIViewController, LocationPermissionsDelegate {
  
     internal var mapView: MapView!
     override public func viewDidLoad() {
         super.viewDidLoad()
         
+//
+//        let myResourceOptions = ResourceOptions(accessToken: "sk.eyJ1IjoicG9sbG93ZjgiLCJhIjoiY2t3ZWRlZW1xMDNtNDJ2cHdzdGE2NGs5ZiJ9.g8IODwKuRVo9a6kAlyyIYQ")
+//        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
+//        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//
+//        self.view.addSubview(mapView)
+//
+//
+///-     my location
+//        mapView.location.delegate = self
+//        mapView.location.options.puckType = .puck2D()
+//
+//
+///-     add custom location
+//        var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(48.85341), longitude: 2.3488))
+//
+//        // Make the annotation show a red pin
+//        pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
+//
+//        // Create the `PointAnnotationManager` which will be responsible for handling this annotation
+//        let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
+//
+//        // Add the annotation to the manager in order to render it on the map.
+//        pointAnnotationManager.annotations = [pointAnnotation]
+//
+//
+//        // Add line between locations
+//
+//        // Define two or more geographic coordinates to connect with a line.
+//        // Line from New York City, NY to Washington, D.C.
+//        let lineCoordinates = [
+//            CLLocationCoordinate2DMake(40.7128, -74.0060),
+//            CLLocationCoordinate2DMake(38.9072, -77.0369)
+//        ]
+//
+///-  Create the line annotation.
+//        var lineAnnotation = PolylineAnnotation(lineCoordinates: lineCoordinates)
+//        lineAnnotation.lineColor = StyleColor(.red)
+//
+//        // Create the `PolylineAnnotationManager` which will be responsible for handling this annotation
+//        let lineAnnnotationManager = mapView.annotations.makePolylineAnnotationManager()
+//
+//        // Add the annotation to the manager.
+//        lineAnnnotationManager.annotations = [lineAnnotation]
         
-        let myResourceOptions = ResourceOptions(accessToken: "sk.eyJ1IjoicG9sbG93ZjgiLCJhIjoiY2t3ZWRlZW1xMDNtNDJ2cHdzdGE2NGs5ZiJ9.g8IODwKuRVo9a6kAlyyIYQ")
-        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
-        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        
+///- gradient line
+        let options = MapInitOptions(styleURI: .light)
+        mapView = MapView(frame: view.bounds, mapInitOptions: options)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(mapView)
          
-        self.view.addSubview(mapView)
+        mapView.mapboxMap.onNext(.mapLoaded) { _ in
+         
+        self.setupExample()
+         
+        // Set the center coordinate and zoom level.
+        let centerCoordinate = CLLocationCoordinate2D(latitude: 38.875, longitude: -77.035)
+        let camera = CameraOptions(center: centerCoordinate, zoom: 12.0)
+        self.mapView.mapboxMap.setCamera(to: camera)
+        }
+        
+        
+        
+    }
+    
+    internal func decodeGeoJSON(from fileName: String) throws -> FeatureCollection? {
+    guard let path = Bundle.main.path(forResource: fileName, ofType: "geojson") else {
+    preconditionFailure("File '\(fileName)' not found.")
+    }
+    let filePath = URL(fileURLWithPath: path)
+    var featureCollection: FeatureCollection?
+    do {
+    let data = try Data(contentsOf: filePath)
+    featureCollection = try JSONDecoder().decode(FeatureCollection.self, from: data)
+    } catch {
+    print("Error parsing data: \(error)")
+    }
+    return featureCollection
+    }
+     
+    internal func setupExample() {
+     
+     
+    // Attempt to decode GeoJSON from file bundled with application.
+    guard let featureCollection = try? decodeGeoJSON(from: "GradientLine") else { return }
+    let geoJSONDataSourceIdentifier = "geoJSON-data-source"
+     
+    // Create a GeoJSON data source.
+    var geoJSONSource = GeoJSONSource()
+    geoJSONSource.data = .featureCollection(featureCollection!)
+    geoJSONSource.lineMetrics = true // MUST be `true` in order to use `lineGradient` expression
+     
+    // Create a line layer
+    var lineLayer = LineLayer(id: "line-layer")
+    lineLayer.filter = Exp(.eq) {
+    "$type"
+    "LineString"
+    }
+     
+    // Setting the source
+    lineLayer.source = geoJSONDataSourceIdentifier
+     
+    // Styling the line
+    lineLayer.lineColor = .constant(StyleColor(.red))
+    lineLayer.lineGradient = .expression(
+    Exp(.interpolate) {
+    Exp(.linear)
+    Exp(.lineProgress)
+    0
+    UIColor.blue
+    0.1
+    UIColor.purple
+    0.3
+    UIColor.cyan
+    0.5
+    UIColor.green
+    0.7
+    UIColor.yellow
+    1
+    UIColor.red
+    }
+    )
+     
+    let lowZoomWidth = 10
+    let highZoomWidth = 20
+    lineLayer.lineWidth = .expression(
+    Exp(.interpolate) {
+    Exp(.linear)
+    Exp(.zoom)
+    14
+    lowZoomWidth
+    18
+    highZoomWidth
+    }
+    )
+    lineLayer.lineCap = .constant(.round)
+    lineLayer.lineJoin = .constant(.round)
+     
+    // Add the source and style layer to the map style.
+    try! mapView.mapboxMap.style.addSource(geoJSONSource, id: geoJSONDataSourceIdentifier)
+    try! mapView.mapboxMap.style.addLayer(lineLayer, layerPosition: nil)
+    }
+    
+    // Selector that will be called as a result of the delegate below
+    func requestPermissionsButtonTapped() {
+        mapView.location.requestTemporaryFullAccuracyPermissions(withPurposeKey: "CustomKey")
     }
     
 }
