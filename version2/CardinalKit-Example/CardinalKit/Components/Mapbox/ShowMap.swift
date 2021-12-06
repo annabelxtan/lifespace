13e8e343f3be55dcf515d1bae5dc9ed3ee2defbf
EstebanRamosFractal <46715376+EstebanRamosFractal@users.noreply.github.com>
2021-12-06 16:46:06 -0500

    add show mapbox heat map

--- version2/CardinalKit-Example/CardinalKit/Components/Mapbox/ShowMap.swift
+++ version2/CardinalKit-Example/CardinalKit/Components/Mapbox/ShowMap.swift
@@ -42,159 +42,38 @@ class ShowMap: UIViewController, LocationPermissionsDelegate {
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
 
         self.view.addSubview(mapView)
-        
+        var allLocations = [CLLocationCoordinate2D]()
         // get firebase points
         JHMapDataManager.shared.getAllMapPoints(onCompletion: {(results) in
             if let results = results as? [mapPoint]{
-                // Create the `PointAnnotationManager` which will be responsible for handling this annotation
-                let pointAnnotationManager = self.mapView.annotations.makePointAnnotationManager()
-                var anotations = [PointAnnotation]()
                 for point in results{
-                    //add custom location
-                    var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: point.latitude, longitude: point.longitude))
-                    //Make the annotation show a red pin
-                    pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
-                    anotations.append(pointAnnotation)
+                    let location = CLLocationCoordinate2D(latitude: point.latitude, longitude: point.longitude)
+                    allLocations.append(location)
                 }
-                pointAnnotationManager.annotations=anotations
+            }
+            do {
+              // Make the GeoJSON source
+              var source = GeoJSONSource()
+                source.data = .feature(Feature(geometry: .lineString(LineString(allLocations))))
+                try self.mapView.mapboxMap.style.addSource(source, id: "SOURCE_ID")
+              var heatLayer = HeatmapLayer(id: "LAYER_ID")
+                heatLayer.source = "SOURCE_ID"
+
+              // Add the layer to the mapView
+                try self.mapView.mapboxMap.style.addLayer(heatLayer)
+                self.mapView.mapboxMap.setCamera(
+                    to: CameraOptions(
+                        center: allLocations[0],
+                        zoom: 9.0
+                    )
+                )
+
+
+            } catch {
+              print("error adding source or layer: \(error)")
             }
         })
-//        mapView.mapboxMap.onNext(.mapLoaded) { _ in
-//            self.addStyleLayers()
-////            self.addVisibilitySwitches()
-//        }
-        let heatMapLayer = HeatmapLayer(id: "heatMap")
-//        heatMapLayer.
-//        let layer = MGLHeatmapStyleLayer(identifier: "earthquake-heat", source: earthquakes)
-//        layer.heatmapWeight = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:(magnitude, 'linear', nil, %@)",
-//                                           [0: 0,
-//                                            6: 1])
-//        layer.heatmapIntensity = NSExpression(format: "mgl_interpolate:withCurveType:parameters:stops:($zoomLevel, 'linear', nil, %@)",
-//                                              [0: 1,
-//                                               9: 3])
-//        mapView.style?.addLayer(layer)
-        
-//
-        
-//
-//
-///-     my location
-//        mapView.location.delegate = self
-//        mapView.location.options.puckType = .puck2D()
-//
-//
-///-     add custom location
-//        var pointAnnotation = PointAnnotation(coordinate: CLLocationCoordinate2D(latitude: CLLocationDegrees(48.85341), longitude: 2.3488))
-//
-//        // Make the annotation show a red pin
-//        pointAnnotation.image = .init(image: UIImage(named: "red_pin")!, name: "red_pin")
-//
-//        // Create the `PointAnnotationManager` which will be responsible for handling this annotation
-//        let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
-//
-//        // Add the annotation to the manager in order to render it on the map.
-//        pointAnnotationManager.annotations = [pointAnnotation]
-//
-//
-//        // Add line between locations
-//
-//        // Define two or more geographic coordinates to connect with a line.
-//        // Line from New York City, NY to Washington, D.C.
-//        let lineCoordinates = [
-//            CLLocationCoordinate2DMake(40.7128, -74.0060),
-//            CLLocationCoordinate2DMake(38.9072, -77.0369)
-//        ]
-//
-///-  Create the line annotation.
-//        var lineAnnotation = PolylineAnnotation(lineCoordinates: lineCoordinates)
-//        lineAnnotation.lineColor = StyleColor(.red)
-//
-//        // Create the `PolylineAnnotationManager` which will be responsible for handling this annotation
-//        let lineAnnnotationManager = mapView.annotations.makePolylineAnnotationManager()
-//
-//        // Add the annotation to the manager.
-//        lineAnnnotationManager.annotations = [lineAnnotation]
-        
-        
-///- gradient line
-//        let options = MapInitOptions(styleURI: .light)
-//        mapView = MapView(frame: view.bounds, mapInitOptions: options)
-//        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        view.addSubview(mapView)
-//
-//        mapView.mapboxMap.onNext(.mapLoaded) { _ in
-//
-//        self.setupExample()
-//
-//        // Set the center coordinate and zoom level.
-//        let centerCoordinate = CLLocationCoordinate2D(latitude: 38.875, longitude: -77.035)
-//        let camera = CameraOptions(center: centerCoordinate, zoom: 12.0)
-//        self.mapView.mapboxMap.setCamera(to: camera)
-//        }
-        
-        
-        
     }
-    
-//    func addStyleLayers() {
-//        // Specify the source IDs. They will be assigned to their respective sources when we
-//        // add the source to the map's style.
-//        let museumSourceId = "museum-source"
-//        let contourSourceId = "contour-source"
-//
-//        // Create a custom vector tileset source. This source contains point features
-//        // that represent museums.
-//        var museumsSource = VectorSource()
-//        museumsSource.url = "mapbox://mapbox.2opop9hr"
-//
-//        var museumLayer = CircleLayer(id: museumLayerId)
-//
-//        // Assign this layer's source.
-//        museumLayer.source = museumSourceId
-//        // Specify the layer within the vector source to render on the map.
-//        museumLayer.sourceLayer = "museum-cusco"
-//
-//        // Use a constant circle radius and color to style the layer.
-//        museumLayer.circleRadius = .constant(8)
-//
-//        // `visibility` is `nil` by default. Set to `visible`.
-//        museumLayer.visibility = .constant(.visible)
-//
-//        let museumColor = UIColor(red: 0.22, green: 0.58, blue: 0.70, alpha: 1.00)
-//        museumLayer.circleColor = .constant(StyleColor(museumColor))
-//
-//        var contourSource = VectorSource()
-//        // Add the Mapbox Terrain v2 vector tileset. Documentation for this vector tileset
-//        // can be found at https://docs.mapbox.com/vector-tiles/reference/mapbox-terrain-v2/
-//        contourSource.url = "mapbox://mapbox.mapbox-terrain-v2"
-//
-//        var contourLayer = LineLayer(id: contourLayerId)
-//
-//        // Assign this layer's source and source layer ID.
-//        contourLayer.source = contourSourceId
-//        contourLayer.sourceLayer = "contour"
-//
-//        // Style the contents of the source's contour layer.
-//        contourLayer.lineCap = .constant(.round)
-//        contourLayer.lineJoin = .constant(.round)
-//
-//        // `visibility` is `nil` by default. Set to `visible`.
-//        contourLayer.visibility = .constant(.visible)
-//        let contourLineColor = UIColor(red: 0.53, green: 0.48, blue: 0.35, alpha: 1.00)
-//        contourLayer.lineColor = .constant(StyleColor(contourLineColor))
-//
-//        let style = mapView.mapboxMap.style
-//
-//        // Add the sources and layers to the map's style.
-//        do {
-//        try style.addSource(museumsSource, id: museumSourceId)
-//        try style.addSource(contourSource, id: contourSourceId)
-//        try style.addLayer(museumLayer)
-//        try style.addLayer(contourLayer)
-//        } catch {
-//            print("Error when adding sources and layers: \(error.localizedDescription)")
-//        }
-//    }
     
     internal func decodeGeoJSON(from fileName: String) throws -> FeatureCollection? {
     guard let path = Bundle.main.path(forResource: fileName, ofType: "geojson") else {
@@ -280,3 +159,112 @@ class ShowMap: UIViewController, LocationPermissionsDelegate {
     }
     
 }
+
+//
+//
+//import MapboxMaps
+//
+//@objc(AnimateImageLayerExample)
+//class AnimateImageLayerExample: UIViewController {
+//    var mapView: MapView!
+//    var sourceId = "radar-source"
+//    var timer: Timer?
+//    var imageNumber = 0
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        let center = CLLocationCoordinate2D(latitude: 41.874, longitude: -75.789)
+//        let cameraOptions = CameraOptions(center: center, zoom: 5)
+//        let mapInitOptions = MapInitOptions(cameraOptions: cameraOptions, styleURI: .dark)
+//        mapView = MapView(frame: view.bounds, mapInitOptions: mapInitOptions)
+//        mapView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+//
+//        // Hide the `scaleBar` at all zoom levels.
+//        mapView.ornaments.options.scaleBar.visibility = .hidden
+//
+//        // This also updates the color of the info button to match the map's style.
+//        mapView.tintColor = .lightGray
+//
+//        // Set the map's `CameraBoundsOptions` to limit the map's zoom level.
+//        try? mapView.mapboxMap.setCameraBounds(with: CameraBoundsOptions(maxZoom: 5.99, minZoom: 4))
+//
+//        view.addSubview(mapView)
+//
+//        mapView.mapboxMap.onNext(.mapLoaded) { _ in
+//            self.addImageLayer()
+//        }
+//    }
+//
+//    func addImageLayer() {
+//        let style = mapView.mapboxMap.style
+//
+//        // Create an `ImageSource`. This will manage the image displayed in the `RasterLayer` as well
+//        // as the location of that image on the map.
+//        var imageSource = ImageSource()
+//
+//        // Set the `coordinates` property to an array of longitude, latitude pairs.
+//        imageSource.coordinates = [
+//            [-80.425, 46.437],
+//            [-71.516, 46.437],
+//            [-71.516, 37.936],
+//            [-80.425, 37.936]
+//        ]
+//
+//        // Get the file path for the first radar image, then set the `url` for the `ImageSource` to that path.
+//        let path = Bundle.main.path(forResource: "radar0", ofType: "gif")!
+//        imageSource.url = path
+//
+//        // Create a `RasterLayer` that will display the images from the `ImageSource`
+//        var imageLayer = RasterLayer(id: "radar-layer")
+//        imageLayer.source = sourceId
+//
+//        // Set `rasterFadeDuration` to `0`. This prevents visible transitions when the image is updated.
+//        imageLayer.rasterFadeDuration = .constant(0)
+//
+//        do {
+//            try style.addSource(imageSource, id: sourceId)
+//            try style.addLayer(imageLayer)
+//
+//            // Add a tap gesture recognizer that will allow the animation to be stopped and started.
+//            let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(manageTimer))
+//            mapView.addGestureRecognizer(tapGestureRecognizer)
+//        } catch {
+//            print("Failed to add the source or layer to style. Error: \(error)")
+//        }
+//        manageTimer()
+//    }
+//
+//    @objc func manageTimer() {
+//        if timer == nil {
+//            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+//                guard let self = self else { return }
+//
+//                // There are five radar images, number 0-4. Increment the count. When that would
+//                // result in an `imageNumber` value greater than 4, reset `imageNumber` to `0`.
+//                if self.imageNumber < 4 {
+//                    self.imageNumber += 1
+//                } else {
+//                    self.imageNumber = 0
+//                }
+//                // Create a `UIImage` from the file at the specified path.
+//                let path = Bundle.main.path(forResource: "radar\(self.imageNumber)", ofType: "gif")
+//                let image = UIImage(contentsOfFile: path!)
+//
+//                do {
+//                    // Update the image used by the `ImageSource`.
+//                    try self.mapView.mapboxMap.style.updateImageSource(withId: self.sourceId, image: image!)
+//                } catch {
+//                    print("Failed to update style image. Error: \(error)")
+//                }
+//            }
+//        } else {
+//            timer?.invalidate()
+//            timer = nil
+//        }
+//    }
+//
+//    deinit {
+//        timer?.invalidate()
+//    }
+//}
