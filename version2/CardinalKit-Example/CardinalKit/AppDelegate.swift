e7ce74a78aae0f0883ec60515127f2dbbc8fed38
Esteban Ramos <julianramos@alternova.co>
2022-03-28 07:20:03 -0500

    schedule survey only if notification is allowed

--- version2/CardinalKit-Example/CardinalKit/AppDelegate.swift
+++ version2/CardinalKit-Example/CardinalKit/AppDelegate.swift
@@ -85,31 +85,32 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         if !UserDefaults.standard.bool(forKey: Constants.prefsNotificationsSchedule) {
             let center = UNUserNotificationCenter.current()
                 center.delegate = self
-            
-            let content = UNMutableNotificationContent()
-            content.title = "Jackson Heart"
-            content.body = "Please Complete Daily Survey"
-            // Configure the recurring date.
-            var dateComponents = DateComponents()
-            dateComponents.calendar = Calendar.current
-
-            dateComponents.hour = 19   // 19:00 hours
-            dateComponents.minute = 00
-
-            // Create the trigger as a repeating event.
-            let trigger = UNCalendarNotificationTrigger(
-                     dateMatching: dateComponents, repeats: true)
-
-            // Create the request
-            let uuidString = UUID().uuidString
-            let request = UNNotificationRequest(identifier: uuidString,
-                        content: content, trigger: trigger)
-
-            // Schedule the request with the system.
-            center.add(request) { (error) in }
             center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
                 if granted {
                     UserDefaults.standard.set(true, forKey: Constants.prefsNotificationsSchedule)
+                    let center = UNUserNotificationCenter.current()
+                        center.delegate = self
+                    
+                    let content = UNMutableNotificationContent()
+                    content.title = "Jackson Heart"
+                    content.body = "Please Complete Daily Survey"
+                    // Configure the recurring date.
+                    var dateComponents = DateComponents()
+                    dateComponents.calendar = Calendar.current
+
+                    dateComponents.hour = 09  // 19:00 hours
+                    dateComponents.minute = 15
+
+                    // Create the trigger as a repeating event.
+                    let trigger = UNCalendarNotificationTrigger(
+                             dateMatching: dateComponents, repeats: true)
+                    // Create the request
+                    let uuidString = UUID().uuidString
+                    let request = UNNotificationRequest(identifier: uuidString,
+                                content: content, trigger: trigger)
+                    
+                    // Schedule the request with the system.
+                    center.add(request) { (error) in }
                 }
             }
         }
