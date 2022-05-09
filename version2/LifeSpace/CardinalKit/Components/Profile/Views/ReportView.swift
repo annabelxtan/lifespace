ad234689d7aa338f79f3a043808d436238be8e9d
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-05-09 17:46:06 -0400

    Update Profile view

--- version2/LifeSpace/CardinalKit/Components/Profile/Views/ReportView.swift
+++ version2/LifeSpace/CardinalKit/Components/Profile/Views/ReportView.swift
@@ -19,7 +19,7 @@ struct ReportView: View {
     
     var body: some View {
         HStack {
-            Text("Report a Problem")
+            Text("Need help?")
             Spacer()
             Text(self.email).foregroundColor(self.color)
         }
